347eb18fd55ef702e52e351966ab31d22b86553a
zhaodongchao <997546127@qq,com>
2017-05-14 17:01:46 +0800

    dcproj-facade模块添加对core,model的依赖

--- dcproj-facade/src/main/java/org/dongchao/facade/webservices/UserServiceImpl.java
+++ dcproj-facade/src/main/java/org/dongchao/facade/webservices/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package org.dongchao.facade.webservices;
 
+import org.dongchao.core.service.UserService;
 import org.dongchao.facade.webservices.interfaces.IUserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.jws.WebService;
 
 /**
  * Created by zhaodongchao on 2017/5/12.
  */
+@Component
 @WebService(endpointInterface = "org.dongchao.facade.webservices.interfaces.IUserFacade")
 public class UserServiceImpl implements IUserFacade {
+    @Autowired
+    private UserService userService;
     @Override
     public String hello() {
         return "haha";
@@ -16,7 +22,6 @@ public class UserServiceImpl implements IUserFacade {
 
     @Override
     public String getUserByname(String username) {
-
-        return "调用过了" + username;
+        return userService.findUserByName(username).getUsername();
     }
 }
