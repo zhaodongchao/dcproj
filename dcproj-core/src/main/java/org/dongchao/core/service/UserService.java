faa039ff4fa71f2100bb3d0e7717342dcdbe3220
zhaodongchao <997546127@qq,com>
2017-05-20 21:09:53 +0800

    添加了密码加密，获取当前登录用户的方法

--- dcproj-core/src/main/java/org/dongchao/core/service/UserService.java
+++ dcproj-core/src/main/java/org/dongchao/core/service/UserService.java
@@ -1,5 +1,10 @@
 package org.dongchao.core.service;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
 import org.dongchao.core.dao.UserDao;
 import org.dongchao.model.common.ResultModel;
 import org.dongchao.model.entity.Permission;
@@ -59,5 +64,26 @@ public class UserService {
         }
         result = new ResultModel(true, "成功找到用户：" + username, user.toString());
         return JsonUtil.Object2JsonString(result);
+    }
+
+    /**
+     * 获取当前session中的登录用户的信息
+     */
+    public User getCurrentUser(){
+        User currentUser = (User) SecurityUtils.getSubject().getPrincipal();
+        return currentUser ;
+    }
+
+    /**
+     * 将用户的密码加密存入数据库
+     */
+    public void encryptPassword(User user) {
+        RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+        user.setSalt(randomNumberGenerator.nextBytes().toHex());
+        String newPassword = new SimpleHash(HASH_ALGORITHM,
+                                            user.getPassword(),
+                                            ByteSource.Util.bytes(user.getSalt()),
+                                            HASH_INTERATIONS).toHex();
+        user.setPassword(newPassword);
     }
 }
