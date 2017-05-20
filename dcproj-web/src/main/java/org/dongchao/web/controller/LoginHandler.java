2d4a897c59c99b736c566512e54435792bf4a298
zhaodongchao <997546127@qq,com>
2017-05-20 21:16:41 +0800

    格式化代码

--- dcproj-web/src/main/java/org/dongchao/web/controller/LoginHandler.java
+++ dcproj-web/src/main/java/org/dongchao/web/controller/LoginHandler.java
@@ -20,9 +20,8 @@ public class LoginHandler {
      * 正真的登录验证是在FormAuthenticationFilter中进行的
      * 由于shiro在登录时会保存上一次的请求路径
      * GET请求用于请求登录页面，POST请求进行身份验证
-     *
      */
-    @RequestMapping(value = {"login"},method = RequestMethod.GET)
+    @RequestMapping(value = {"login"}, method = RequestMethod.GET)
     public String login() {
         return "login";
     }
@@ -33,7 +32,7 @@ public class LoginHandler {
      * 因此会导致就算登录失败，也不会提示失败，且会以登录成功的方式处理。也就是访问 /login post ，会执行到下面方法
      * 也就是说，只要是请求为loginUrl method=POST时， 不管MyFormAuthenticationFilter验证成功或者失败都会执行上一次的请求及表单提交的POST请求地址
      */
-    @RequestMapping(value = {"login"},method = RequestMethod.POST)
+    @RequestMapping(value = {"login"}, method = RequestMethod.POST)
     public ModelAndView success(HttpServletRequest request) {
         ModelAndView mv;
         if (null != request.getAttribute(MyFormAuthenticationFilter.DEFAULT_ERROR_KEY_ATTRIBUTE_NAME)) {
@@ -51,17 +50,15 @@ public class LoginHandler {
             }
             return mv;
         }
-//        User currentUser = (User) SecurityUtils.getSubject().getPrincipal();
-
         mv = new ModelAndView("home");
         return mv;
     }
 
     /**
-     *  默认打开系统主页，所有人都能访问，但是主页相关的类容和操作，会根据游客和用户权限经行控制显示
+     * 默认打开系统主页，所有人都能访问，但是主页相关的类容和操作，会根据游客和用户权限经行控制显示
      */
     @RequestMapping(method = RequestMethod.GET)
-    public String defaultPage(){
-        return "home" ;
+    public String defaultPage() {
+        return "home";
     }
 }
