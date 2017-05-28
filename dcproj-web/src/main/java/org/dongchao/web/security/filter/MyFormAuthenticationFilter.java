dd2ead7072e1bde783e4e2852c0ca12bb5d44254
zhaodongchao <997546127@qq,com>
2017-05-28 09:39:16 +0800

    禁止了用户权限cache,修改了loginfilter的实现

---
+++ dcproj-web/src/main/java/org/dongchao/web/security/filter/MyFormAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package org.dongchao.web.security.filter;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletRequest;
+
+/**
+ * 1、onLoginFailure 时 把异常添加到request attribute中 而不是异常类名
+ * 2、登录成功时：成功页面重定向：
+ * 2.1、如果前一个页面是登录页面，-->2.3
+ * 2.2、如果有SavedRequest 则返回到SavedRequest
+ * 2.3、否则根据当前登录的用户决定返回到管理员首页/前台首页
+ * Created by zhaodongchao on 2017/5/14.
+ */
+public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
+    private static final Logger logger = LoggerFactory.getLogger(MyFormAuthenticationFilter.class);
+
+    @Override
+    protected void setFailureAttribute(ServletRequest request, AuthenticationException ae) {
+        request.setAttribute(getFailureKeyAttribute(), ae);
+    }
+
+    @Override
+    public String getSuccessUrl() {
+
+        return super.getSuccessUrl();
+    }
+}
